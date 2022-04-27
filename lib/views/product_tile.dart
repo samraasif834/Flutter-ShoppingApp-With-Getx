ea60bbd68bd03521f8065f404bddbf95b4d4e7fb
samraasif834 <samraasif834@gmail.com>
2022-04-27 20:12:20 +0500

    update

---
+++ lib/views/product_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingapp/models/product.dart';
+
+class ProductTile extends StatelessWidget {
+  final Product product;
+  const ProductTile(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
